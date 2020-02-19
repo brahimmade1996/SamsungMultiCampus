5572e09655906ba5c6351cf46665142b447cc74a
Namho Jeong <jnh7807@gmail.com>
2020-02-19 17:58:14 +0900

    Create urls.py

---
+++ WebServer/mysite/memo/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views 
+from django.shortcuts import redirect
+
+urlpatterns = [
+    path('', views.index),
+    path('<category>/<int:pk>/<mode>', views.MemoView.as_view(), name = 'Memo'),
+    path('', lambda request: redirect('memo', 'common', 0, 'list')),
+]
