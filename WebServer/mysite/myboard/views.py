f9116c7bf1e295229f2e765086f9468c521c14cf
Namho Jeong <jnh7807@gmail.com>
2020-02-20 17:48:19 +0900

    Update views.py

--- WebServer/mysite/myboard/views.py
+++ WebServer/mysite/myboard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404,redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.generic import View
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
@@ -9,10 +9,55 @@ from django import forms
 from . import models
 from . import forms
 from . import apps
+from django.core.paginator import Paginator
 
 # url: bolg -> index함수 실행
 def index(request):
     return HttpResponse('ok')
+
+# get방식으로 하기
+# 지워달라는 요청, 특정 데이터를 요청할 수 있음
+def ajaxdel(request):
+    pk = request.GET.get('pk')
+    board = models.Board.objects.all().get(pk = pk)
+    # db에서 지우기
+    # board.delete()  # 연습할 때는 진짜 db가 지워지지 않도록 주석처리하기
+    return JsonResponse({'error': '0'})
+
+
+# 특정 페이지의 모든 데이터 
+def ajaxget(request):
+    page = request.GET.get('page', 1)
+    datas = models.Board.objects.all().filter(category = 'common')  # data가 없으면 queryset은 None을 return
+    print(datas)
+    # page개념이 아니므로 슬라이싱으로 해도 상관없음.-> len==0일 때 경고 나오게 하려면 슬라이싱으로 해야 함.
+    page = int(page)
+    subs = datas[(page-1)*3: (page*3)]
+    # p = Paginator(datas, 3)
+    # subs = p.page(page)
+
+    datas = {'datas': [{'pk': data.pk, 'title': data.title, 'cnt': data.cnt} for data in subs]}
+    return JsonResponse(datas, json_dumps_params = {'ensure_ascii': False})  # 한글 오류 -> utf-8로 인코딩됨
+
+
+
+def page(request):
+    datas = [{'id': 1, 'name':  '홍길동1'},
+            {'id': 2, 'name':  '홍길동2'},
+            {'id': 3, 'name':  '홍길동3'},
+            {'id': 4, 'name':  '홍길동4'},
+            {'id': 5, 'name':  '홍길동5'},
+            {'id': 6, 'name':  '홍길동6'},
+            {'id': 7, 'name':  '홍길동7'},
+            ]
+
+    page = request.GET.get('page', 1)  #default: 1
+    p = Paginator(datas, 3)# collections 형태의 데이터,  # 한 페이지에 보여줄 데이터 개수
+    sub = p.page(page) # 몇번째 페이지를 가지고 올 지 
+                    # 슬라이싱의 개념과 같음
+
+    return render(request, 'myboard/page.html', {'datas': sub})
+
 
 ### <int:pk>/ <mode>를 url을 사용하기 위해
 class BoardView(View) :
@@ -27,8 +72,14 @@ class BoardView(View) :
             username = request.session.get('username', '')
             user = User.objects.get(username =username)
             data= models.Board.objects.all().filter(author = user, category = category)
-            context = {'data': data,'username': username, 'category': category}
-            return render (request, apps.APP + '/list.html', context)
+            
+            # page 개념
+            page = request.GET.get('page', 1)  
+            p = Paginator(data, 3)
+            subs = p.page(page) 
+            
+            context = {'datas': subs,'username': username, 'category': category}
+            return render (request, apps.APP + '/pagelist.html', context)
 
         elif mode =='detail':
             p = get_object_or_404(models.Board, pk = pk)
