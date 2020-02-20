6e35ac3870a376cc10c4057969c1e71e955b959c
Namho Jeong <jnh7807@gmail.com>
2020-02-20 17:48:16 +0900

    Update urls.py

--- WebServer/mysite/myboard/urls.py
+++ WebServer/mysite/myboard/urls.py
@@ -3,8 +3,11 @@ from . import views
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', views.index),
+    # path('', views.index),
+    path('', views.page),
+    path('ajaxdel', views.ajaxdel),
+    path('ajaxget', views.ajaxget),
+
     path('<category>/<int:pk>/<mode>', views.BoardView.as_view(), name = 'myboard'),
-    path('', lambda request: redirect('myboard', 'common', 0, 'list'),
-    )
-]
+    # path('', lambda request: redirect('myboard', 'common', 0, 'list'),
+    ]
