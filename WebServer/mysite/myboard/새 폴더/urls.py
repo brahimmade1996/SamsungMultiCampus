4fc6da87f90cf4964dfec152608a28dc7aa597b5
Namho Jeong <jnh7807@gmail.com>
2020-02-20 17:48:44 +0900

    Create urls.py

---
+++ WebServer/mysite/myboard/새 폴더/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views 
+from django.shortcuts import redirect
+
+urlpatterns = [
+    # path('', views.index),
+    path('', views.page),
+    path('ajaxdel', views.ajaxdel),
+    path('ajaxget', views.ajaxget),
+
+    path('<category>/<int:pk>/<mode>', views.BoardView.as_view(), name = 'myboard'),
+    # path('', lambda request: redirect('myboard', 'common', 0, 'list'),
+    ]
